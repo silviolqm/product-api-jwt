77da8631ee103e1a581e90e4941d0769dbffa56f
silviolqm <silviolqm@gmail.com>
2025-08-14 12:06:31 -0300

    Project creation

---
+++ src/src/main/java/io/github/silviolqm/product_api_jwt/ProductApiJwtApplication.java
@@ -0,0 +1,13 @@
+package io.github.silviolqm.product_api_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApiJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApiJwtApplication.class, args);
+	}
+
+}
---
+++ src/src/test/java/io/github/silviolqm/product_api_jwt/ProductApiJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.silviolqm.product_api_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApiJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
